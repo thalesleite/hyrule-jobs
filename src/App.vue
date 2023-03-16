a06485d653217c30a3787ada8b70d501e7d1c99b
thalesleite <thalesaleite@gmail.com>
2023-03-16 11:24:22 +0000

    setup the project and create the Jobs interface with some data

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="app">
+    <p>{{ jobs[0].location }}</p>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent, ref } from "vue"
+import Job from "./types/Job"
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-});
+  name: "App",
+  components: {},
+  setup() {
+    const jobs = ref<Job[]>([
+      {
+        title: "Farm worker",
+        location: "Lon Lon Ranch",
+        salary: 30000,
+        id: "1",
+      },
+      {
+        title: "Quarryman",
+        location: "Death Mountain",
+        salary: 40000,
+        id: "2",
+      },
+      {
+        title: "Flute Player",
+        location: "The lost woods",
+        salary: 35000,
+        id: "3",
+      },
+      {
+        title: "Fisherman",
+        location: "Lake Hylia",
+        salary: 21000,
+        id: "4",
+      },
+      {
+        title: "Prison Guard",
+        location: "Gerudo Valley",
+        salary: 32000,
+        id: "5",
+      },
+    ])
+
+    return { jobs }
+  },
+})
 </script>
 
 <style>
