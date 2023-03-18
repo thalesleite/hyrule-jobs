6d5fb4a90b755cd3d986eb8fc454c2b002d8e2c0
thalesleite <thalesaleite@gmail.com>
2023-03-18 16:13:35 +0000

    add jobs, buttons order by, icons, and font style

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,29 @@
 <template>
   <div class="app">
-    <p>{{ jobs[0].location }}</p>
+    <header>
+      <div class="title">
+        <img src="./assets/heart.svg" alt="site logo" />
+        <h1>Hyrule Jobs</h1>
+      </div>
+      <div class="order">
+        <button @click="handleClick('title')">Order by title</button>
+        <button @click="handleClick('salary')">Order by salary</button>
+        <button @click="handleClick('location')">Order by location</button>
+      </div>
+    </header>
+    <JobsList :jobs="jobs" :order="order" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from "vue"
 import Job from "./types/Job"
+import OrderTerm from "./types/OrderTerm"
+import JobsList from "./components/JobsList.vue"
 
 export default defineComponent({
   name: "App",
-  components: {},
+  components: { JobsList },
   setup() {
     const jobs = ref<Job[]>([
       {
@@ -44,8 +57,13 @@ export default defineComponent({
         id: "5",
       },
     ])
+    const order = ref<OrderTerm>("title")
 
-    return { jobs }
+    const handleClick = (term: OrderTerm) => {
+      order.value = term
+    }
+
+    return { jobs, handleClick, order }
   },
 })
 </script>
@@ -58,5 +76,33 @@ export default defineComponent({
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+header {
+  text-align: center;
+}
+header .order {
+  margin-top: 20px;
+}
+button {
+  margin: 0 10px;
+  color: #1195c9;
+  border: 3px solid #1195c9;
+  background: #d5f0ff;
+  padding: 8px 16px;
+  border-radius: 4px;
+  cursor: pointer;
+  font-weight: bold;
+}
+header .title {
+  display: flex;
+  justify-content: center;
+}
+header img {
+  width: 60px;
+  margin-right: 20px;
+}
+header h1 {
+  font-size: 3em;
 }
 </style>
